58cc0ccbf6bd2a04bc9e0d5c385db77985f39838
Tien Pham <tien.000.pro@gmail.com>
2022-11-19 23:03:34 +0700

    add more homework

--- baitapArrowFunc/js/index.js
+++ baitapArrowFunc/js/index.js
@@ -16,11 +16,21 @@ const domId = (id) => document.getElementById(id);
 const loadButton = () => {
   const html = colorList.reduce((total, element) => {
     total += `
-            <button class="color-button ${element}"></button>
+            <button id="btn_${element}" onclick="changeColor(event)" class="color-button ${element}"></button>
         `;
     return total;
   }, "");
   domId("colorContainer").innerHTML = html;
+};
+
+const changeColor = (e) => {
+  for (let element of colorList) {
+    domId(`btn_${element}`).classList.remove("active");
+    domId("house").classList.remove(`${element}`);
+  }
+  let activeHouse = e.target.classList[1];
+  domId("house").classList.add(activeHouse);
+  e.target.classList.add("active");
 };
 
 window.onload = () => {
